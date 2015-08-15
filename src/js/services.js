094b31d806ddb38535424fdadd677236c066c037
Camden Rudisill <camden.rudisill@richmond.edu>
2015-08-15 14:01:07 -0400

    Update: JQuery function for mobile

--- src/js/services.js
+++ src/js/services.js
@@ -33,6 +33,41 @@ $(function(){
 		$("#salon").removeClass('tab-active');
 	});
 
+	// Mobile
+
+	$("#salon-list-mobile").hide();
+	$("#wedding-list-mobile").hide();
+	$("#hair-mobile").addClass('tab-active');
+
+	$("#hair-mobile").click(function(){
+
+		$("#hair-list-mobile").fadeIn('fast');
+		$("#salon-list-mobile").hide();
+		$("#wedding-list-mobile").hide();
+		$("#hair-mobile").addClass('tab-active');
+		$("#salon-mobile").removeClass('tab-active');
+		$("#wedding-mobile").removeClass('tab-active');
+
+	});
+
+	$("#salon-mobile").click(function(){
+		$("#salon-list-mobile").fadeIn('fast');
+		$("#hair-list-mobile").hide();
+		$("#wedding-list-mobile").hide();
+		$("#salon-mobile").addClass('tab-active');
+		$("#hair-mobile").removeClass('tab-active');
+		$("#wedding-mobile").removeClass('tab-active');
+	});
+
+	$("#wedding").click(function(){
+		$("#wedding-list-mobile").fadeIn('fast');
+		$("#hair-list-mobile").hide();
+		$("#salon-list-mobile").hide();
+		$("#wedding-mobile").addClass('tab-active');
+		$("#hair-mobile").removeClass('tab-active');
+		$("#salon-mobile").removeClass('tab-active');
+	});
+
 
 });
 
