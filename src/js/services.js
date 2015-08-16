64a25e66883c5c8b73eeb49794b3c60d0e57376a
Camden Rudisill <camden.rudisill@richmond.edu>
2015-08-15 20:59:57 -0400

    Update: refactor services.js

--- src/js/services.js
+++ src/js/services.js
@@ -1,79 +1,25 @@
-$(function(){
+$(function() {
 
+	// start state 
 	$("#salon-list").hide();
 	$("#wedding-list").hide();
 	$("#hair").addClass('tab-active');
-
-	// mobile
-
 	$("#mobile-salon-list").hide();
 	$("#mobile-wedding-list").hide();
 	$("#mobile-hair").addClass('tab-active');
 
-	$("#hair").click(function(){
+	// Display chosen list when clicked on
+	$('.services-nav-li').click(displayServices);
 
-		$("#hair-list").fadeIn('fast');
-		$("#salon-list").hide();
-		$("#wedding-list").hide();
-		$("#hair").addClass('tab-active');
-		$("#salon").removeClass('tab-active');
-		$("#wedding").removeClass('tab-active');
 
-	});
-
-	$("#salon").click(function(){
-		$("#salon-list").fadeIn('fast');
-		$("#hair-list").hide();
-		$("#wedding-list").hide();
-		$("#salon").addClass('tab-active');
-		$("#hair").removeClass('tab-active');
-		$("#wedding").removeClass('tab-active');
-	});
-
-	$("#wedding").click(function(){
-		$("#wedding-list").fadeIn('fast');
-		$("#hair-list").hide();
-		$("#salon-list").hide();
-		$("#wedding").addClass('tab-active');
-		$("#hair").removeClass('tab-active');
-		$("#salon").removeClass('tab-active');
-	});
-
-	// Mobile
-
-	$("#mobile-salon-list").hide();
-	$("#mobile-wedding-list").hide();
-	$("#mobile-hair").addClass('tab-active');
-
-	$("#mobile-hair").click(function(){
-
-		$("#mobile-hair-list").fadeIn('fast');
-		$("#mobile-salon-list").hide();
-		$("#mobile-wedding-list").hide();
-		$("#mobile-hair").addClass('tab-active');
-		$("#mobile-salon").removeClass('tab-active');
-		$("#mobile-wedding").removeClass('tab-active');
-
-	});
-
-	$("#mobile-salon").click(function(){
-		$("#mobile-salon-list").fadeIn('fast');
-		$("#mobile-hair-list").hide();
-		$("#mobile-wedding-list").hide();
-		$("#mobile-salon").addClass('tab-active');
-		$("#mobile-hair").removeClass('tab-active');
-		$("#mobile-wedding").removeClass('tab-active');
-	});
-
-	$("#mobile-wedding").click(function(){
-		$("#mobile-wedding-list").fadeIn('fast');
-		$("#mobile-hair-list").hide();
-		$("#mobile-salon-list").hide();
-		$("#mobile-wedding").addClass('tab-active');
-		$("#mobile-hair").removeClass('tab-active');
-		$("#mobile-salon").removeClass('tab-active');
-	});
+	function displayServices() {
+		var id = '#' + this.id;
+		var listId = id + '-list';
+		$(id).addClass('tab-active');
+		$(listId).fadeIn('fast');
+		$('.services-content-ul').not(listId).hide();
+		$('.services-nav-li').not(id).removeClass('tab-active');
+	}
 
 
 });
-
